07c9e2a35fa7e2ab3fc5e54478cc80d7c7323850
Sandra Munyanshoza - home <sandra.munyanshoza@icloud.com>
2023-11-16 02:45:39 +0100

    clang-format on all files

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,55 +1,54 @@
 #ifndef __SERVER_HPP__
-# define __SERVER_HPP__
+#define __SERVER_HPP__
 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <fstream>
 #include "utils/server_utils.hpp"
 #include "utils/utils.hpp"
+#include <arpa/inet.h>
+#include <fstream>
+#include <netinet/in.h>
+#include <sys/types.h>
 
 class Server {
 
 private:
+  std::string _host;
+  std::string _server_name;
+  std::string _root;
+  std::string _index;
+  int _client_max_body_size;
+  std::vector<int> _ports;
+  std::vector<int> _sockets;
+  std::map<std::string, t_location> _location_list;
 
-	std::string									_host;
-	std::string									_server_name;
-	std::string									_root;
-	std::string									_index;
-	int											_client_max_body_size;
-	std::vector<int>							_ports;
-	std::vector<int>							_sockets;
-	std::map<std::string, t_location>			_location_list;
-
-	bool 										parseOption(const int &, std::string &, std::stringstream &, const std::string &, std::vector<int> &);
-	void										addDefaultLocation();
+  bool parseOption(const int &, std::string &, std::stringstream &,
+                   const std::string &, std::vector<int> &);
+  void addDefaultLocation();
 
 public:
+  Server();
+  ~Server();
 
-	Server();
-	~Server();
+  std::string getHost() const;
+  std::string getServerName() const;
+  std::string getRoot() const;
+  std::string getIndex() const;
+  int getBodySize() const;
+  std::vector<int> getPorts() const;
+  std::map<std::string, t_location> getLocationlist() const;
+  std::vector<int> getSockets() const;
 
-	std::string									getHost() const;
-	std::string 								getServerName() const;
-	std::string 								getRoot() const;
-	std::string 								getIndex() const;
-	int											getBodySize() const;
-	std::vector<int> 							getPorts() const;
-	std::map<std::string, t_location> 			getLocationlist() const;
-	std::vector<int>							getSockets() const;
+  bool setHost(const std::string &);
+  bool setServerName(const std::string &);
+  bool setRoot(std::string &);
+  bool setIndex(const std::string &);
+  bool setBodySize(const std::string &);
+  bool addPort(const std::string &, std::vector<int> &);
+  bool addLocation(std::stringstream &, std::string &value);
+  void addSocket(int &);
 
-	bool										setHost(const std::string&);
-	bool										setServerName(const std::string&);
-	bool										setRoot(std::string&);
-	bool										setIndex(const std::string&);
-	bool										setBodySize(const std::string&);
-	bool										addPort(const std::string&, std::vector<int>&);
-	bool										addLocation(std::stringstream&, std::string &value);
-	void										addSocket(int&);
-
-	bool										parseServer(const std::string &, const std::string &, std::vector<int> &, std::vector<std::string> &);
-	void										handleRequest(struct t_request &, std::vector<std::string> &, bool &);
-
+  bool parseServer(const std::string &, const std::string &, std::vector<int> &,
+                   std::vector<std::string> &);
+  void handleRequest(struct t_request &, std::vector<std::string> &, bool &);
 };
 
 #endif
--- include/utils/server_utils.hpp
+++ include/utils/server_utils.hpp
@@ -1,46 +1,49 @@
 #ifndef __SERVER_UTILS_HPP__
-# define __SERVER_UTILS_HPP__
+#define __SERVER_UTILS_HPP__
 
-#include <sstream>
-#include <vector>
+#include "utils.hpp"
+#include <dirent.h>
 #include <iostream>
 #include <map>
+#include <sstream>
 #include <unistd.h>
-#include <dirent.h>
-#include "utils.hpp"
+#include <vector>
 
-# define DISPLAY_METHOD_AND_LOCATION true
+#define DISPLAY_METHOD_AND_LOCATION true
 
-# define DIGITS "0123456789"
+#define DIGITS "0123456789"
 
 struct t_location {
 
-	std::string					location;
-	std::string					root;
-	std::string					index;
-	std::vector<std::string>	methods;
-	std::string					autoindex;
-	bool						valid;
-
+  std::string location;
+  std::string root;
+  std::string index;
+  std::vector<std::string> methods;
+  std::string autoindex;
+  bool valid;
 };
 
 namespace server_utils {
 
-    std::string getOptionName(const std::string &);
-    std::string getOptionValue(const std::string &);
-    std::string getLocationBlock(std::stringstream &);
-    t_location  newLocation(const std::string &, const std::string &);
-    void        ft_error(int, std::string, std::string);
-	bool		allowedMethod(std::string &, std::vector<std::string> &);
-	void		setRequest(t_request &, bool &);
-	void		checkUrl(struct t_request &, std::vector<std::string> &);
-	void		checkLocation(struct t_request &, std::map<std::string, struct t_location> &);
-	void		sendTable(struct t_request &, std::string);
-	void		addParentDirectory(std::string &, std::string);
-	void		addLinkList(std::string &, std::string);
+std::string getOptionName(const std::string &);
+std::string getOptionValue(const std::string &);
+std::string getLocationBlock(std::stringstream &);
+t_location newLocation(const std::string &, const std::string &);
+void ft_error(int, std::string, std::string);
+bool allowedMethod(std::string &, std::vector<std::string> &);
+void setRequest(t_request &, bool &);
+void checkUrl(struct t_request &, std::vector<std::string> &);
+void checkLocation(struct t_request &,
+                   std::map<std::string, struct t_location> &);
+void sendTable(struct t_request &, std::string);
+void addParentDirectory(std::string &, std::string);
+void addLinkList(std::string &, std::string);
 
-	class invalidMethodException : public std::exception { public: virtual const char *what() const throw() { return "Invalid method"; } };
-
+class invalidMethodException : public std::exception {
+public:
+  virtual const char *what() const throw() { return "Invalid method"; }
 };
+
+}; // namespace server_utils
 
 #endif
